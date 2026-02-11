e934e61852b9cb2e464faa77fa745f68d2c2cc9f
NiranjanaYuvaraj08 <niranjanay.23csd@kongu.edu>
2026-02-11 11:07:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ami-0317b0f0a0144b137" 
+  ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.small"
-  key_name      = "abc"
+  key_name      = "mak_1"
 
-  security_groups = [aws_security_group.mygame.name]
+  security_groups = [aws_security_group.mak.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "mygame" {
-  name = "mygame"
+resource "aws_security_group" "mak" {
+  name = "mak"
 
   ingress {
     from_port   = 22
